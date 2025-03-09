1e94a3a511a43f8c9430bc919a364e7c92b94c7b
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2025-03-09 01:32:42 -0300

    Primer commit - Proyecto Cine

---
+++ src/main/java/com/cineapp/cine_app/CineAppApplication.java
@@ -0,0 +1,13 @@
+package com.cineapp.cine_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineapp/cine_app/CineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineapp.cine_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
